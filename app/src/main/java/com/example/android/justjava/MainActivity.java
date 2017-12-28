159ecb5c9bbfe05f9ea8cc71abc43776445a2f08
tr0lczyk <matt.olczyk@gmail.com>
2017-12-28 11:08:20 +0100

    string addition

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -49,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
      * method called when order button is clicked
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        String message = "Item count" + 5;
+        displayMessage(message);
     }
 
     /**
@@ -66,6 +67,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 
